4fbaf8dbba0bd93a15b0cc15962531f6a814d8fb
harrym123-glitch <harrymeng2021@gmail.com>
2026-06-03 17:01:33 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,9 @@
-// 第一步：去 Firebase 控制台创建项目
-// 第二步：创建 Firestore Database
-// 第三步：把你的 Web App 配置粘贴到这里
-
-export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyCoOXPUyu0OJxyLIBNUZRhzofRrAXH6mQ",
+  authDomain: "visit-booking-web.firebaseapp.com",
+  projectId: "visit-booking-web",
+  storageBucket: "visit-booking-web.firebasestorage.app",
+  messagingSenderId: "629760485189",
+  appId: "1:629760485189:web:387c0e7371cc772eda402c",
+  measurementId: "G-5L0R7NQG9E"
 };
-
-// 后台测试账号，可自行修改
-export const ADMIN_USERNAME = "admin";
-export const ADMIN_PASSWORD = "123456";
